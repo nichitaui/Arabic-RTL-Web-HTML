dae6842064b99e43e114455a3517529e61397caf
smarty17 <smart.design17@outlook.com>
2020-03-30 04:27:42 +0300

    questionnaire issue fix & dropdown issue fix

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -6,3 +6,26 @@ $(document).on('keyup', '.form-control', function(e) {
         $(this).closest('.form-group').removeClass('invalid');
     }
 });
+
+
+
+
+
+
+// single select dropdown js
+$(document).on('click', '.single-dropdown .dropdown-item', function() {
+    $(this).closest('.single-dropdown').find('.dropdown-toggle').text($(this).text());
+});
+  
+// multi select dropdown js
+$(document).on('click', '.multiple-dropdown .dropdown-item input[type=checkbox]', function() {
+    var checked_checkbox_arr = $(this).closest('.multiple-dropdown').find('.dropdown-item input[type=checkbox]:checked');
+    if (checked_checkbox_arr.length > 1) {
+        var selected = 'יותר (' + checked_checkbox_arr.length + ')';
+        $(this).closest('.multiple-dropdown').find('.dropdown-toggle').text(selected);
+    } else if (checked_checkbox_arr.length == 1) {
+        $(this).closest('.multiple-dropdown').find('.dropdown-toggle').text($(this).closest('.dropdown-item').text());
+    } else {
+        $(this).closest('.multiple-dropdown').find('.dropdown-toggle').text('סוג מוצר');
+    }
+});
