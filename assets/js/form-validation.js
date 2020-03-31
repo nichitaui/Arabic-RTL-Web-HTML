f819801b53f23576114481f44b86c89811cdc178
smarty17 <smart.design17@outlook.com>
2020-04-01 01:00:25 +0300

    multiple-dropdown autocomplete

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -16,16 +16,26 @@ $(document).on('keyup', '.form-control', function(e) {
 $(document).on('click', '.single-dropdown .dropdown-item', function() {
     $(this).closest('.single-dropdown').find('.dropdown-toggle').val($(this).text());
 });
-// $(document).on('focus', '.single-dropdown .dropdown-toggle', function() {
-//     $(this).attr('type', 'text');
-// });
-$(document).on('keyup', '.single-dropdown .dropdown-toggle[type=text]', function() {
-    console.log($(this).val());
+
+$(document).on('focus', '.input-group-prepend .dropdown-toggle[type=text]', function() {
+    $(this).attr("readonly", false);
+});
+
+$(document).on('focusout', '.input-group-prepend .dropdown-toggle[type=text]', function() {
+    $(this).attr("readonly", true);
+});
+
+$(document).on('keyup', '.input-group-prepend .dropdown-toggle[type=text]', function() {
     var keywoard = $(this).val();
-    var item_arr = $(this).closest('.single-dropdown').find('.dropdown-menu').find('.dropdown-item').show();
+    var item_arr = $(this).closest('.input-group-prepend').find('.dropdown-menu').find('.dropdown-item');
     for (var i=0; i<item_arr.length; i++) {
         if (!item_arr[i].textContent.includes(keywoard)) {
             $(item_arr[i]).hide();
+        } else {
+            $(item_arr[i]).show();
+        }
+        if ($(item_arr[i]).find('[type=checkbox]')) {
+            $(item_arr[i]).find('[type=checkbox]').prop('checked', false);
         }
     }
 });
@@ -35,10 +45,10 @@ $(document).on('click', '.multiple-dropdown .dropdown-item input[type=checkbox]'
     var checked_checkbox_arr = $(this).closest('.multiple-dropdown').find('.dropdown-item input[type=checkbox]:checked');
     if (checked_checkbox_arr.length > 1) {
         var selected = 'יותר (' + checked_checkbox_arr.length + ')';
-        $(this).closest('.multiple-dropdown').find('.dropdown-toggle').text(selected);
+        $(this).closest('.multiple-dropdown').find('.dropdown-toggle')[0].value = selected;
     } else if (checked_checkbox_arr.length == 1) {
-        $(this).closest('.multiple-dropdown').find('.dropdown-toggle').text($(this).closest('.dropdown-item').text());
+        $(this).closest('.multiple-dropdown').find('.dropdown-toggle')[0].value = checked_checkbox_arr[0].closest('.dropdown-item').textContent;
     } else {
-        $(this).closest('.multiple-dropdown').find('.dropdown-toggle').text('סוג מוצר');
+        $(this).closest('.multiple-dropdown').find('.dropdown-toggle')[0].value = '';
     }
 });
