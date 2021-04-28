910c0a324fd0f1062c4dc111d231b842d138a196
smarty17 <smart.design17@outlook.com>
2021-04-28 10:52:13 +0800

    customer-list: stick rate bar

---
+++ assets/custom/rate-color-bar/rate-color-bar.js
@@ -0,0 +1,17 @@
+initRateColorBar();
+
+function initRateColorBar() {
+    [].forEach.call(document.getElementsByClassName('rate-color-bar'), function(v,i,a) {
+        console.log('v ', v);
+        console.log('i ', i);
+        console.log('a ', a);
+        v.innerHTML += '<div class="bar-row">' +
+                            '<div class="min-val">5</div>' +
+                            '<div class="bar-container">' +
+                                '<div class="bar-stick"></div>' +
+                                '<div class="bar-target" alt="" style="left:'+ (25 * (parseFloat(v.dataset.value) - 1)) +'%;"></div>' +
+                            '</div>' + 
+                            '<div class="max-val">1</div>' +
+                        '</div>';
+    });
+}
