02f3841099a2a234ac296eb94c80c1669829847d
smarty17 <smart.design17@outlook.com>
2021-06-02 22:18:19 +0800

    v3 - simulator main

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,7 @@
 $(document).on('click', '#btn_toggle_edit_app_header_form', function() {
     $('#app_header_form').toggleClass('editable');
 });
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
